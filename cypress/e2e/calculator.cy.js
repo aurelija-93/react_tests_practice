4187135e17b8b79e04e400a36d88d48c4ab9fa4e
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-07-17 16:05:32 +0100

    passed 3rd integration test

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -50,4 +50,83 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '21')
   })
+
+  it('should be able to show a positive output', () => {
+    cy.get('#number4').click();
+    cy.get('#operator-add').click();
+    cy.get('#number7').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '11')
+  })
+
+  it('should be able to show a negative output', () => {
+    cy.get('#number4').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number7').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-3')
+  })
+
+  it('should be able to show a decimal output', () => {
+    cy.get('#number4').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number1').click();
+    cy.get('#decimal').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '2.8')
+  })
+
+  // this test doesn't work in cypress:
+
+  // it('should be able to show a very large number', () => {
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#operator-multiply').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#number9').click();
+  //   cy.get('#operator-equals').click();
+  //   cy.get('.display').should('contain', '9.99999999998e+23')
+
+  // version for cypress:
+
+  it('should be able to show a very large number', () => {
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '999998000001')
+  })
 })
