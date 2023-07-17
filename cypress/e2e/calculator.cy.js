55fbd65efb03526d2fb7553187f824135bebf48e
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-07-17 15:23:47 +0100

    passed 1st integration test

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -7,4 +7,37 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should be able to perform addition', () => {
+    cy.get('#number1').click();
+    cy.get('#operator-add').click();
+    cy.get('#number4').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '5')
+  })
+
+  it('should be able to perform subtraction', () => {
+    cy.get('#number7').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number4').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '3')
+  })
+
+  it('should be able to perform multiplication', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number5').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '15')
+  })
+
+  it('should be able to perform division', () => {
+    cy.get('#number2').click();
+    cy.get('#number1').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number7').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '3')
+  })
 })
